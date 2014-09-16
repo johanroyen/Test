96932d713c510502fa5fca26935e093e8c037843
Johan Royen Larsson <johan.royen@gmail.com>
2014-09-16 09:41:46 +0200

    Initial commit from local git

---
+++ GitHubTest/JRLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JRLAppDelegate.h
+//  GitHubTest
+//
+//  Created by Johan Royen Larsson on 9/16/14.
+//  Copyright (c) 2014 Boboshi Applikationer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JRLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHubTest
+//
+//  Created by Johan Royen Larsson on 9/16/14.
+//  Copyright (c) 2014 Boboshi Applikationer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JRLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JRLAppDelegate class]));
+    }
+}
